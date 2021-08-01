1a410dcc5ff4091b27022df4e740a2112f19cd52
kodoku2010 <dnantonov@stud.etu.ru>
2021-08-01 18:02:15 +0300

    Update tests

--- pages/cryptoveche.py
+++ pages/cryptoveche.py
@@ -1,21 +1,22 @@
-import os
-
 from pages.base import WebPage
 from pages.elements import WebElement
 
 
-class LoginPage(WebPage):
-
-    # email = input('Please enter your email: ')
-    # password = input('Please enter your password: ')
-    # new_password = input('Please enter your new password: ')
-
-    select_btn = WebElement(id='auth_type_select')
-    observer_btn = WebElement(css_selector='div[data-type="/observer_login"]')
-    username_field = WebElement(id='username')
-    password_field = WebElement(id='password')
-    submit_btn = WebElement(id='auth-btn')
-    new_password_field = WebElement(id='new_password')
-    new_password_field2 = WebElement(id='repeat_new_password')
-    save_btn = WebElement(css_selector='div.profile-settings-block-save-button')
-
+class MainPage(WebPage):
+    select_role_btn = WebElement(id='auth_type_select')  # Select role for auth
+    observer_btn = WebElement(css_selector='div[data-type="/observer_login"]')  # Observer locator
+    username_field = WebElement(id='username')  # Username locator
+    password_field = WebElement(id='password')  # Password locator
+    submit_btn = WebElement(id='auth-btn')  # Submit auth form locator
+    new_password_field = WebElement(id='new_password')  # New password field locator
+    new_password_field2 = WebElement(id='repeat_new_password')  # Repeat new password field locator
+    save_btn = WebElement(css_selector='div.profile-settings-block-save-button')  # Save new password locator
+    add_poll = WebElement(id='main-btn')  # Add new poll button locator
+    poll_title = WebElement(id='feTitle')  # Poll title locator
+    voters = WebElement(id='is_voters_expandable')  # Expandable voters locator
+    voters_open = WebElement(css_selector='div[data-type="open"]:nth-child(2)')  # Open voters locator
+    add_question = WebElement(css_selector='div.add-question')  # Add new question locator
+    question_type = WebElement(css_selector='div.selectBtn[data-type=""]')  # Choose question type locator
+    question_by_choice = WebElement(css_selector='div[data-type="ynq"]')  # Question by choice type locator
+    question_title = WebElement(css_selector='input.add-agenda-question-name-text-field')  # Question title locator
+    add_event_button = WebElement(id='add_event')  # Add new poll button locator
--- tests/test_cryptoveche.py
+++ tests/test_cryptoveche.py
@@ -16,8 +16,7 @@
 #  pytest -v --driver Remote --capability browserName chrome tests/*
 #
 import time
-
-from pages.cryptoveche import LoginPage
+from pages.cryptoveche import MainPage
 
 email = input('Please enter your email: ')
 password = input('Please enter your password: ')
@@ -25,15 +24,39 @@ new_password = input('Please enter your new password: ')
 
 
 def test_login_as_observer_and_change_password(browser):
-    page = LoginPage(browser, 'https://admin.evote74.dltc.spbu.ru/admin_auth')
-    page.select_btn.click()
-    page.observer_btn.click()
-    page.username_field = email
-    page.password_field = password
-    page.submit_btn.click()
-    time.sleep(2)
+    """ Login as observer, go to settings page and change password"""
+    page = MainPage(browser, 'https://admin.evote74.dltc.spbu.ru/admin_auth')
+    page.select_role_btn.click()  # Click select role button
+    page.observer_btn.click()  # Choose observer button
+    page.username_field = email  # Input email in the form field
+    page.password_field = password  # Input password in the form field
+    page.submit_btn.click()  # Click bubmit form button
+    time.sleep(2)  # Wait until form will be submit
+    # Open profile settings page
     page.get(f'https://admin.evote74.dltc.spbu.ru/users/{email}')
-    browser.implicitly_wait(10)
-    page.new_password_field = new_password
-    page.new_password_field2 = new_password
-    page.save_btn.click()
+    browser.implicitly_wait(10)  # Wait until new_password_field will be acceptable
+    page.new_password_field = new_password  # Input new password
+    page.new_password_field2 = new_password  # Repeat new password
+    page.save_btn.click()  # Click save button
+
+
+def test_create_new_poll(browser):
+    """ Login as admin and create new poll"""
+    page = MainPage(browser, 'https://admin.evote74.dltc.spbu.ru/admin_auth')
+    page.username_field = email  # Input email in the form field
+    page.password_field = new_password  # Input password in the form field
+    page.submit_btn.click()  # Click bubmit form button
+    time.sleep(2)  # Wait until form will be submit
+    page.get('https://admin.evote74.dltc.spbu.ru/main')  # Open main page
+    browser.implicitly_wait(5)  # Wait until add_poll button will be displayed
+    page.add_poll.click()  # Click add poll button
+    page.poll_title = 'Test poll title'  # Input poll title
+    page.voters.click()  # Choose poll voters type
+    page.voters_open.click()  # Click open poll voters type
+    page.add_question.click()  # Click add question button
+    page.question_type.click()  # Choose question type
+    page.question_by_choice.click()  # Click question by choice type button
+    page.question_title = 'Test question title'  # Input question title
+    page.add_event_button.click()  # Click submit new poll button
+    time.sleep(2)  # Wait until form will be submit
+
